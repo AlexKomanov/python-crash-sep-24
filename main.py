d8341d88a5377e1a498157f24f0f700470b8340e
AlexKomanov <akomanov88@gmail.com>
2024-09-03 00:02:15 +0300

    second commit

---
+++ main.py
@@ -0,0 +1,18 @@
+first_number = 5
+second_number = 10
+# print(first_number + second_number) # comment
+
+print(type('Hello World!'))
+print(type(15))
+print(type(15.3))
+print(type(True))
+print(type(False))
+
+my_list = [45, 56, 78, 7, 100]
+print(type(my_list))
+
+my_tuple = (45, 56, 78, 7, 1000)
+print(type(my_tuple))
+
+print(type({"name": "Alex", "age": 36}))
+
